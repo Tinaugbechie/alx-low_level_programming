2b6544bfca5efd21b85659b274361aa4953f6dbf
Tinaugbechie <ugbechiet@gmail.com>
2023-03-17 04:41:26 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-*main - entry point
-*
-*Return: always 0 
-*/
+  * main - main function
+  *
+  * Return: always 0
+  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
